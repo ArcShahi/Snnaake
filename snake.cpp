b87539bb0d4f206bccbbc0c7ce4c9a7ea0df3932
ArcShahi <arcshahi54@gmail.com>
2024-10-17 22:25:48 +0530

    Refactoring

--- snake.cpp
+++ snake.cpp
@@ -9,154 +9,158 @@ constexpr int WIDTH = 1200;
 constexpr int HEIGHT = 900;
 
 enum class GameState {
-     PLAYING,
-     GAME_OVER
+	PLAYING,
+	GAME_OVER
 };
-
-
 
 
 struct Vec2
 {
-     int x, y;
-     Vec2 operator+(const Vec2& other) const { return { x + other.x, y + other.y }; }
-     bool operator==(const Vec2& other) const { return x == other.x && y == other.y; }
+	int x, y;
+	Vec2 operator+(const Vec2& other) const { return { x + other.x, y + other.y }; }
+	bool operator==(const Vec2& other) const { return x == other.x && y == other.y; }
 };
 
-struct Snake
+
+class Snake
 {
-     std::deque<Vec2> body;
-     Vec2 direction{ -1, 0 };  // Starting direction: left
-     int score{ 0 };
-     int girth{ 10 };
+public:
+	Snake()
+		:direction{ -1,0 }, score{ 0 }, girth{ 10 }
+	{}
+
+public:
+	std::deque<Vec2> body{ {60, 15}, {61, 15}, {62, 15}, {63, 15}, {64, 15},
+					 {65, 15}, {66, 15}, {67, 15}, {68, 15}, {69, 15} };
+
+	Vec2 direction{  };  // Starting direction: left
+	int score{ 0 };
+	int girth{ 10 };
+
 };
 
-Snake initializeSnake() {
-     return Snake{
-         std::deque<Vec2>{{60, 15}, {61, 15}, {62, 15}, {63, 15}, {64, 15},
-                          {65, 15}, {66, 15}, {67, 15}, {68, 15}, {69, 15}},
-         {-1, 0},  // Initial direction: left
-         0, 10
-     };
-}
+
+
 
 Vec2 generateFood(int girth) {
-     return {
-         arc::randomi(1, WIDTH / girth - 1),
-         arc::randomi(1, HEIGHT / girth - 1)
-     };
+	return {
+	    arc::randomi(1, WIDTH / girth - 1),
+	    arc::randomi(1, HEIGHT / girth - 1)
+	};
 }
 
 int main()
 {
-     InitWindow(WIDTH, HEIGHT, "Snake");
-     SetTargetFPS(30);
+	InitWindow(WIDTH, HEIGHT, "Snake");
+	SetTargetFPS(30);
 
-     GameState gameState = GameState::PLAYING;
-     Snake snake = initializeSnake();
-     Vec2 food = generateFood(snake.girth);
-     HideCursor();
-     while (!WindowShouldClose())
-     {
-          switch (gameState)
-          {
-          case GameState::PLAYING:
-          {
-               // Input handling
-               Vec2 newDirection = snake.direction;
-               if ((IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) && snake.direction.x == 0)
-                    newDirection = { -1, 0 };
-               if ((IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) && snake.direction.x == 0)
-                    newDirection = { 1, 0 };
-               if ((IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)) && snake.direction.y == 0)
-                    newDirection = { 0, -1 };
-               if ((IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) && snake.direction.y == 0)
-                    newDirection = { 0, 1 };
+	GameState gameState = GameState::PLAYING;
 
-               snake.direction = newDirection;
+	Snake snake;
+	Vec2 food = generateFood(snake.girth);
+	HideCursor();
+	while (!WindowShouldClose())
+	{
+		switch (gameState)
+		{
+		case GameState::PLAYING:
+		{
+			// Input handling
+			Vec2 newDirection = snake.direction;
+			if ((IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) && snake.direction.x == 0)
+				newDirection = { -1, 0 };
+			if ((IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) && snake.direction.x == 0)
+				newDirection = { 1, 0 };
+			if ((IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)) && snake.direction.y == 0)
+				newDirection = { 0, -1 };
+			if ((IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN)) && snake.direction.y == 0)
+				newDirection = { 0, 1 };
 
-               // Move snake
-               Vec2 newHead = snake.body.front() + snake.direction;
-               snake.body.push_front(newHead);
+			snake.direction = newDirection;
 
-               // Check for food collision
-               if (newHead == food)
-               {
-                    snake.score++;
-                    food = generateFood(snake.girth);
-                    // Snake grows, so we don't remove the tail
-               }
-               else
-               {
-                    // Remove tail if no food was eaten
-                    snake.body.pop_back();
-               }
+			// Move snake
+			Vec2 newHead = snake.body.front() + snake.direction;
+			snake.body.push_front(newHead);
 
-               // Check for wall collision
-               if (newHead.x < 0 || newHead.x >= WIDTH / snake.girth ||
-                    newHead.y < 0 || newHead.y >= HEIGHT / snake.girth)
-               {
-                    gameState = GameState::GAME_OVER;
-               }
+			// Check for food collision
+			if (newHead == food)
+			{
+				snake.score++;
+				food = generateFood(snake.girth);
+				// Snake grows, so we don't remove the tail
+			}
+			else
+			{
+				// Remove tail if no food was eaten
+				snake.body.pop_back();
+			}
 
-               // Check for self-collision
-               for (size_t i = 1; i < snake.body.size(); ++i)
-               {
-                    if (newHead == snake.body[i])
-                    {
-                         gameState = GameState::GAME_OVER;
-                         break;
-                    }
-               }
-               break;
-          }
-          case GameState::GAME_OVER:
-          {
-               if (IsKeyPressed(KEY_ENTER))
-               {
-                    // Restart the game
-                    snake = initializeSnake();
-                    food = generateFood(snake.girth);
-                    gameState = GameState::PLAYING;
-               }
-               break;
-          }
-          }
+			// Check for wall collision
+			if (newHead.x < 0 || newHead.x >= WIDTH / snake.girth ||
+				newHead.y < 0 || newHead.y >= HEIGHT / snake.girth)
+			{
+				gameState = GameState::GAME_OVER;
+			}
 
-          // Rendering
-          BeginDrawing();
-          ClearBackground(BLACK);
+			// Check for self-collision
+			for (size_t i = 1; i < snake.body.size(); ++i)
+			{
+				if (newHead == snake.body[i])
+				{
+					gameState = GameState::GAME_OVER;
+					break;
+				}
+			}
+			break;
+		}
+		case GameState::GAME_OVER:
+		{
+			if (IsKeyPressed(KEY_ENTER))
+			{
+				// Restart the game
+				snake = Snake();
+				food = generateFood(snake.girth);
+				gameState = GameState::PLAYING;
+			}
+			break;
+		}
+		}
 
-          // Draw food
-          DrawRectangle(food.x * snake.girth, food.y * snake.girth, snake.girth, snake.girth, WHITE);
+		// Rendering
+		BeginDrawing();
+		ClearBackground(BLACK);
 
-          // Draw snake
-          for (size_t i = 0; i < snake.body.size(); ++i)
-          {
-               Color color = (i == 0) ? (gameState == GameState::GAME_OVER ? BLUE : YELLOW) : (gameState == GameState::GAME_OVER ? BLACK : RED);
+		// Draw food
+		DrawRectangle(food.x * snake.girth, food.y * snake.girth, snake.girth, snake.girth, WHITE);
 
-               DrawRectangle(snake.body[i].x * snake.girth, snake.body[i].y * snake.girth,
-                    snake.girth, snake.girth, color);
-          }
+		// Draw snake
+		for (size_t i = 0; i < snake.body.size(); ++i)
+		{
+			Color color = (i == 0) ? (gameState == GameState::GAME_OVER ? BLUE : ORANGE) :
+				(gameState == GameState::GAME_OVER ? BLACK : RED);
 
-          // Draw score
-          DrawText(("SCORE: " + std::to_string(snake.score)).c_str(), 950, 10, 20, WHITE);
+			DrawRectangle(snake.body[i].x * snake.girth, snake.body[i].y * snake.girth,
+				snake.girth, snake.girth, color);
+		}
 
-          // Draw border
-          DrawRectangleLines(0, 0, WIDTH, HEIGHT, gameState == GameState::GAME_OVER ? RED : GREEN);
+		// Draw score
+		DrawText(("SCORE: " + std::to_string(snake.score)).c_str(), 950, 10, 20, WHITE);
 
-          DrawText("ArcShahi", 1000, 870, 20, RED);
+		// Draw border
+		DrawRectangleLines(0, 0, WIDTH, HEIGHT, gameState == GameState::GAME_OVER ? RED : GREEN);
 
-          // Draw game over message and restart prompt
-          if (gameState == GameState::GAME_OVER)
-          {
-               DrawText("GAME OVER", WIDTH / 2 - 100, HEIGHT / 2 - 30, 40, RED);
-               DrawText("\t\t\tPress ENTER to restart", WIDTH / 2 - 150, HEIGHT / 2 + 20, 20, WHITE);
-          }
+		DrawText("ArcShahi", 1050, 870, 20, RED);
 
-          EndDrawing();
-     }
+		// Draw game over message and restart prompt
+		if (gameState == GameState::GAME_OVER)
+		{
+			DrawText("GAME OVER", WIDTH / 2 - 100, HEIGHT / 2 - 30, 40, RED);
+			DrawText("\t\t\tPress ENTER to restart", WIDTH / 2 - 150, HEIGHT / 2 + 20, 20, WHITE);
+		}
 
-     CloseWindow();
-     return 0;
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
 }
